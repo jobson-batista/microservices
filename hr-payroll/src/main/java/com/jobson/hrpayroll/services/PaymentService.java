2bcaaa9f72fec440bf034920a20ed8331445ef95
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-03-18 13:18:10 -0300

    Comunication become projects implemented

--- hr-payroll/src/main/java/com/jobson/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/jobson/hrpayroll/services/PaymentService.java
@@ -1,13 +1,29 @@
 package com.jobson.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.jobson.hrpayroll.entities.Payment;
+import com.jobson.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Jobson", 200.0, days);
+		Map<String, String> uriVaribles = new HashMap<>();
+		uriVaribles.put("id", ""+workerId);
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVaribles);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
