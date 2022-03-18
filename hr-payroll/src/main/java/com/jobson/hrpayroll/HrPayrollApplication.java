e525218920a3d99f296a8062a8d4d70fdad542e1
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-03-18 14:47:03 -0300

    Ribbon load balance

--- hr-payroll/src/main/java/com/jobson/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/jobson/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,12 @@ package com.jobson.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
+@SpringBootApplication
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
--- hr-worker/src/main/java/com/jobson/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/jobson/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.jobson.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import com.jobson.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private WorkerRepository workerRepository;
@@ -27,6 +35,9 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
